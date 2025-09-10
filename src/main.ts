e5c821d5a3fc399a4bb47763bf7af23d6a8a9041
Kandriws <61208102+Kandriws@users.noreply.github.com>
2025-09-10 18:22:03 +0200

    chore: update morgan logging configuration based on environment

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import { GlobalExceptionsFilter } from './shared/infrastructure/exceptions/globa
 import { setupSwagger } from './shared/infrastructure/config/swagger.config';
 import * as morgan from 'morgan';
 import helmet from 'helmet';
+import { envs } from './shared/infrastructure/config/env.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
-
   app.useGlobalPipes(CustomValidationPipe);
   app.useGlobalFilters(new GlobalExceptionsFilter());
   app.enableCors({
@@ -18,7 +18,7 @@ async function bootstrap() {
     methods: 'GET,POST,PUT,PATCH,DELETE',
     credentials: true,
   });
-  app.use(morgan('dev'));
+  app.use(envs.app.isDevelopment ? morgan('dev') : morgan('combined'));
   app.setGlobalPrefix('api');
 
   setupSwagger(app);
