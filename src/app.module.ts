aeda59d82731e4e8b39a3419d97e0539684d95e0
Kandriws <61208102+Kandriws@users.noreply.github.com>
2025-09-10 17:23:16 +0200

    feat: add throttling and security middleware with helmet to enhance API protection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,28 @@ import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 import { JwtAuthGuard } from './auth/infrastructure/guards';
 import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 
 @Module({
-  imports: [AuthModule, SharedModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60,
+        limit: 20,
+      },
+    ]),
+    AuthModule,
+    SharedModule,
+  ],
   controllers: [],
   providers: [
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import { CustomValidationPipe } from './shared/infrastructure/pipes/validation.p
 import { GlobalExceptionsFilter } from './shared/infrastructure/exceptions/global-exceptions.filter';
 import { setupSwagger } from './shared/infrastructure/config/swagger.config';
 import * as morgan from 'morgan';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   app.useGlobalPipes(CustomValidationPipe);
   app.useGlobalFilters(new GlobalExceptionsFilter());
